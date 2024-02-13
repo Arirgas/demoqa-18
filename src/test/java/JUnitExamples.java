14ff9ee2a8d2bcc100a862f4579d8aa5eac7c3e1
a.tumanov <a.tumanov@free-lance.ru>
2024-02-13 19:15:51 +0300

    Add JUnitExamples.java

--- src/test/java/JUnitExamples.java
+++ src/test/java/JUnitExamples.java
@@ -1,5 +1,4 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class JUnitExamples {
 
@@ -8,12 +7,29 @@ public class JUnitExamples {
     static void beforeAll() {
         System.out.println("Here is BeforeAll()");
     }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("    Here is beforeEach()");
+    }
+
+    @AfterAll
+
+    static void AfterAll() {
+        System.out.println("Here is AfterAll()");
+    }
+
+    @AfterEach
+    void AfterEach() {
+        System.out.println("Here is AfterEach()");
+    }
+
     @Test
     void firsTest(){
-        System.out.println("Here is firstTest()");
+        System.out.println("    Here is firstTest()");
     }
     @Test
     void secondTest(){
-        System.out.println("Here is secondTest()");
+        System.out.println("        Here is secondTest()");
     }
 }
